253703cbe90082f1e48d71609478f680f3a3f520
mencerspoore <spencerscottmoore@gmail.com>
2016-10-27 16:58:51 -0400

    Added a Mecanum class
    has no purpose and not complete

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/PushbotTeleopPOV_Linear.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/PushbotTeleopPOV_Linear.java
@@ -89,7 +89,7 @@ public class PushbotTeleopPOV_Linear extends LinearOpMode {
             // Run wheels in POV mode (note: The joystick goes negative when pushed forwards, so negate it)
             // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
             left  = -gamepad1.left_stick_y + gamepad1.right_stick_x;
-            right = -gamepad1.left_stick_y - gamepad1.right_stick_x;
+            right = -gamepad1.left_stick_y - gamepad1.right_stick_x;z
 
             // Normalize the values so neither exceed +/- 1.0
             max = Math.max(Math.abs(left), Math.abs(right));
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/season2016_17/teleop/MecanumDrive.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.season2016_17.teleop;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.season2016_17.RobotHardware;
+
+/**
+ * Created by Robotics on 10/27/2016.
+ */
+public class MecanumDrive {
+
+    RobotHardware robot;
+    Telemetry telemetry;
+
+    public void mecDriveJoystick(double joystickX, double joystickY) {
+        mecDriveAngle(Math.atan2(joystickY,joystickX), Math.sqrt(joystickX*joystickX+joystickY*joystickY));
+        // Good method for finding angle of joystick
+        // Math.toDegrees(Math.atan2(y,x));
+    }
+
+    public void mecDriveAngle(double angle, double power){
+        double v1 = power * Math.sin(angle+Math.PI/4) + 1;
+        double v2 = power * Math.cos(angle + Math.PI / 4) - 1;
+        double v3 = power * Math.cos(angle+Math.PI/4) + 1;
+        double v4 = power * Math.sin(angle+Math.PI/4) - 1;
+
+    }
+}
